078407f1299fe049b726bc3476d3ec1ae50c2b68
James Phillips <James.Phillips@ibm.com>
2019-07-12 09:51:45 -0500

    builds post correctly, currently prints to console

--- foodConnect/src/app/donorpost/donorpost.component.ts
+++ foodConnect/src/app/donorpost/donorpost.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DonorPost } from './donorpost';
 @Component({
   selector: 'app-donorpost',
   templateUrl: './donorpost.component.html',
   styleUrls: ['./donorpost.component.scss']
 })
-export class DonorpostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DonorpostComponent {
+  donorpost = new DonorPost('','','','',0,0,'','',false,'','','');
+  onSubmit(){
+    console.log(this.donorpost.pickupAddress+'\n'+
+    this.donorpost.foodType+'\n'
+    +this.donorpost.foodDesc+'\n'
+    +this.donorpost.consumable+'\n'
+    +this.donorpost.allergenInfo+'\n'
+    +this.donorpost.foodWeight+'\n'
+    +this.donorpost.foodValue+'\n'
+    +this.donorpost.reasonForDonation+'\n'
+    +this.donorpost.pickupTime+'\n'
+    +this.donorpost.foodInspected+'\n'
+    +this.donorpost.allergenInfo+'\n');
   }
 
 }
